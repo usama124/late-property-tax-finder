9ec6863b2578ef9e21b7479da8d0a536c1e2d9e6
Usama Tahir <utahir.itp@sparkcognition.com>
2022-03-12 20:17:55 +0500

    Updated chromedriver and chrome_options

--- src/SeleniumDriver.py
+++ src/SeleniumDriver.py
@@ -16,8 +16,10 @@ class WebDriver:
         CHROME_DRIVER_PATH = "ChromDriver/chromedriver"
         driver = None
         chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        chrome_options.add_argument('--disable-gpu')
         chrome_options.binary_location = self.chrome_path
         chrome_options.add_argument("User-Agent=" + str(ua.random))
         chrome_options.add_argument(f'Referer=https://www.google.com/')
